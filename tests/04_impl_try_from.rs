d9da859505327db7a999cebe83c104cc7c215911
Frank Laranja <laranja@riseup.net>
2025-05-24 14:18:23 +0100

    documentation changed

--- tests/04_impl_try_from.rs
+++ tests/04_impl_try_from.rs
@@ -1,20 +1,18 @@
 use core::convert::TryFrom;
 
 nodyn::wrap! {
-  /// A test of the `wrap!` macro
-  #[derive(PartialEq, Debug)]
-  pub Foo<'a> {
-    i64,
-    &'a str,
-    u32,
-    [u8;4],
-  }
-
+    #[derive(PartialEq, Debug)]
+    pub Foo<'a> {
+        i64,
+        &'a str,
+        u32,
+        [u8;4],
+    }
 }
 
 fn main() {
-    let t2: Foo = 66u32.into();
-    assert_eq!(t2, Foo::U32(66));
+    let t2: Foo = 42u32.into();
+    assert_eq!(t2, Foo::U32(42));
     let r2 = u32::try_from(t2);
-    assert_eq!(r2, Ok(66u32));
+    assert_eq!(r2, Ok(42u32));
 }
